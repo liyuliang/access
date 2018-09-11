0fe7f891b48d6207ec394f207b4bd7939741b83c
liyuliang <liyuliang2013@gmail.com>
2018-09-11 21:23:36 +0800

    convert object to json byte

--- byte.go
+++ byte.go
@@ -2,13 +2,16 @@ package access
 
 import (
 	"reflect"
+	"strconv"
+	"strings"
 )
 
-func ToByte(obj interface{}) (data []byte) {
+func ToByte(obj interface{}) []byte {
 	targetObj := reflect.ValueOf(obj)
 	structElem := targetObj.Elem()
 	structType := structElem.Type()
 
+	result := `{`
 	for i := 0; i < structElem.NumField(); i++ {
 		fieldName := structType.Field(i).Name
 		structField := structElem.FieldByName(fieldName)
@@ -17,40 +20,48 @@ func ToByte(obj interface{}) (data []byte) {
 
 		case "string":
 			fieldValue := structField.String()
-			println(fieldValue)
+			result += `"` + fieldName + `":"` + fieldValue + `",`
 		case "int":
-			fieldValue := structField.Int()
-			println(fieldValue)
+			fieldValue := structField.Interface().(int)
+			result += `"` + fieldName + `":"` + strconv.Itoa(fieldValue) + `",`
 			break
 		case "int32":
 			fieldValue := structField.Interface().(int32)
-			println(fieldValue)
+			result += `"` + fieldName + `":"` + strconv.FormatInt(int64(fieldValue), 10) + `",`
 			break
 		case "int64":
 			fieldValue := structField.Interface().(int64)
-			println(fieldValue)
+			result += `"` + fieldName + `":"` + strconv.FormatInt(fieldValue, 10) + `",`
 			break
 		case "float32":
 			fieldValue := structField.Interface().(float32)
-			println(fieldValue)
+			result += `"` + fieldName + `":"` + strconv.FormatFloat(float64(fieldValue), 'f', -1, 32) + `",`
 			break
 		case "float64":
 			fieldValue := structField.Interface().(float64)
-			println(float64(fieldValue))
+			result += `"` + fieldName + `":"` + strconv.FormatFloat(fieldValue, 'f', -1, 64) + `",`
 			break
 		case "[]string":
 			fieldValue := structField.Interface().([]string)
-			for key, value := range fieldValue {
-				println(key, value)
+			result += `"` + fieldName + `":[`
+			for _, value := range fieldValue {
+				result += `"` + value + `",`
 			}
+			result += "],"
 			break
 		case "map[string]string":
 			fieldValue := structField.Interface().(map[string]string)
+			result += `"` + fieldName + `":[`
 			for key, value := range fieldValue {
-				println(key, value)
+				result += `{"` + key + `":"` + value + `"},`
 			}
+			result += "],"
 			break
 		}
 	}
-	return
+
+	result += `}`
+	result = strings.Replace(result,",]","]",-1)
+	result = strings.Replace(result,",}","}",-1)
+	return []byte(result)
 }
--- byte_test.go
+++ byte_test.go
@@ -7,26 +7,36 @@ func TestToByte(t *testing.T) {
 	type people struct {
 		Name     string
 		Age      int
-		Money    float64
+		Money1    int32
+		Money2    int64
+		Money3    float32
+		Money4    float64
 		Websites []string
 		Account  map[string]string
 	}
 	p := new(people)
 	p.Name = "liyuliang"
 	p.Age = 25
-	p.Money = 3333.33
+	p.Money1 = 31
+	p.Money2 = 25
+	p.Money3 = 3333.33
+	p.Money4 = 3333.33
 	p.Websites = []string{
 		"http://1",
 		"http://2",
 		"http://3",
 		"http://4",
 	}
+
 	p.Account = make(map[string]string)
 	p.Account["ICBC"] = "100"
 	p.Account["IKBC"] = "20"
 	p.Account["CCBC"] = "300"
-	ToByte(p)
-	//b := ToByte(p)
-	//println(string(b))
+	data := ToByte(p)
+
+	expect := `{"Name":"liyuliang","Age":"25","Money1":"31","Money2":"25","Money3":"3333.33","Money4":"3333.33","Websites":["http://1","http://2","http://3","http://4"],"Account":[{"ICBC":"100"},{"IKBC":"20"},{"CCBC":"300"}]}`
+	if string(data) != string(expect) {
+		t.Error("Object to byte json wrong, expect is ", string(expect), "\n but get :", string(data))
+	}
 
 }
