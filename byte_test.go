d281580994f1ec67936ae73c8dd004628e24be1f
liyuliang <liyuliang2013@gmail.com>
2018-09-12 22:56:43 +0800

    fixed map and struct wrong format

--- byte_test.go
+++ byte_test.go
@@ -19,7 +19,7 @@ func TestToByte(t *testing.T) {
 		Websites []string
 		Account  map[string]string
 		MyCar1   *Car
-		MyCar2   Car
+		MyCar2   *Car
 	}
 
 	c1 := new(Car)
@@ -50,11 +50,11 @@ func TestToByte(t *testing.T) {
 	p.Account["IKBC"] = "20"
 	p.Account["CCBC"] = "300"
 	p.MyCar1 = c1
-	p.MyCar2 = c2
+	p.MyCar2 = &c2
 
 	data := ToByte(p)
 
-	expect := `{"Name":"liyuliang","Age":"25","Money1":"31","Money2":"25","Money3":"3333.33","Money4":"3333.33","Websites":["http://1","http://2","http://3","http://4"],"Account":[{"ICBC":"100"},{"IKBC":"20"},{"CCBC":"300"}],"MyCar1":"","MyCar2":""}`
+	expect := `{"Name":"liyuliang","Age":"25","Money1":"31","Money2":"25","Money3":"3333.33","Money4":"3333.33","Websites":["http://1","http://2","http://3","http://4"],"Account":{"ICBC":"100","IKBC":"20","CCBC":"300"},"MyCar1":"{"Brand":"AAAA","Price":"930382.33"}","MyCar2":"{"Brand":"BBBBB","Price":"1828881.28"}"}`
 	if string(data) != string(expect) {
 		t.Error("Object to byte json wrong, expect is ", string(expect), "\n but get :", string(data))
 	}
